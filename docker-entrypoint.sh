cef52f2b061cf055de968db47be3253b4aa96fc1
Nick Potts <github@nickpotts.com.au>
2025-10-16 23:24:10 -0300

    and more

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,57 +5,7 @@ set -e
 : ${TRACKING_DOMAIN:="localhost"}
 : ${POSTHOG_HOST:="us.i.posthog.com"}
 : ${POSTHOG_ASSETS_HOST:="us-assets.i.posthog.com"}
-: ${SUBPATH:=""}
-: ${CORS_ENABLED:="false"}
-: ${CORS_ORIGIN:="https://${TRACKING_DOMAIN}"}
 : ${SSL_ENABLED:="true"}
-
-# Generate CORS block if enabled
-if [ "${CORS_ENABLED}" = "true" ]; then
-    CORS_BLOCK="header {
-		Access-Control-Allow-Origin ${CORS_ORIGIN}
-	}
-"
-else
-    CORS_BLOCK=""
-fi
-
-# Generate handlers based on whether subpath is used
-if [ -n "${SUBPATH}" ]; then
-    # Using subpath - use handle_path to strip the prefix
-    SUBPATH_HANDLERS="handle_path ${SUBPATH}/static* {
-		rewrite * /static{path}
-		reverse_proxy https://${POSTHOG_ASSETS_HOST}:443 {
-			header_up Host ${POSTHOG_ASSETS_HOST}
-			header_down -Access-Control-Allow-Origin
-		}
-	}
-
-	handle_path ${SUBPATH}* {
-		rewrite * {path}
-		reverse_proxy https://${POSTHOG_HOST}:443 {
-			header_up Host ${POSTHOG_HOST}
-			header_down -Access-Control-Allow-Origin
-		}
-	}
-"
-else
-    # No subpath - use the simple version
-    SUBPATH_HANDLERS="handle /static {
-		reverse_proxy https://${POSTHOG_ASSETS_HOST}:443 {
-			header_up Host ${POSTHOG_ASSETS_HOST}
-			header_down -Access-Control-Allow-Origin
-		}
-	}
-
-	handle {
-		reverse_proxy https://${POSTHOG_HOST}:443 {
-			header_up Host ${POSTHOG_HOST}
-			header_down -Access-Control-Allow-Origin
-		}
-	}
-"
-fi
 
 # Configure domain with or without SSL
 if [ "${SSL_ENABLED}" = "false" ]; then
@@ -65,19 +15,14 @@ else
 fi
 
 # Export variables for envsubst
-export DOMAIN_CONFIG TRACKING_DOMAIN POSTHOG_HOST POSTHOG_ASSETS_HOST SUBPATH CORS_BLOCK SUBPATH_HANDLERS
+export DOMAIN_CONFIG TRACKING_DOMAIN POSTHOG_HOST POSTHOG_ASSETS_HOST
 
 # Substitute environment variables in the Caddyfile template
 echo "Configuring Caddy with:"
 echo "  TRACKING_DOMAIN: ${TRACKING_DOMAIN}"
 echo "  POSTHOG_HOST: ${POSTHOG_HOST}"
 echo "  POSTHOG_ASSETS_HOST: ${POSTHOG_ASSETS_HOST}"
-echo "  SUBPATH: ${SUBPATH:-'(none)'}"
 echo "  SSL_ENABLED: ${SSL_ENABLED}"
-echo "  CORS_ENABLED: ${CORS_ENABLED}"
-if [ "${CORS_ENABLED}" = "true" ]; then
-    echo "  CORS_ORIGIN: ${CORS_ORIGIN}"
-fi
 
 envsubst < /etc/caddy/Caddyfile.template > /etc/caddy/Caddyfile
 
